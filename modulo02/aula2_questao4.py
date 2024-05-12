09d3a638f800359f318d7646f1a129ca7b39a1a1
Paulo Henrique <juniorhenrique.1@hotmail.com>
2024-05-11 21:51:07 -0300

    Update aula2_questao4.py

--- modulo02/aula2_questao4.py
+++ modulo02/aula2_questao4.py
@@ -1,6 +1,7 @@
 saldo = 500
-rendimento = saldo * 1.01
-rendimento = rendimento * 1.01
-rendimento = rendimento * 1.01
+juros = 1.01
+saldo = saldo * juros
+saldo = saldo * juros
+saldo = saldo * juros
 print("Após 3 meses meu novo saldo é")
-print(rendimento)
+print(saldo)
