a9b3fd62179527a987322cf036e0f2ca1c012cb1
Paulo Henrique <juniorhenrique.1@hotmail.com>
2024-05-12 21:14:40 -0300

    v01

---
+++ modulo03/aula4_questao1.py
@@ -0,0 +1,8 @@
+n1 = float(input("Digite o numero 1:"))
+n2 = float(input("Digite o numero 2:"))
+if (n1 + n2) % 2 == 0:
+    print("Numero par")
+else:
+    print("Numero impar")
+    
+
---
+++ modulo03/aula4_questao2.py
@@ -0,0 +1,7 @@
+nota = int(input("Informe a avaliação do filme em uma escala de 1 a 5: "))
+
+if nota == 5: print("Excelente!")
+if nota == 4: print("Muito Bom!")
+if nota == 3: print("Bom!")
+if nota == 2: print("Regular.")
+if nota == 1: print("Ruim.1")
