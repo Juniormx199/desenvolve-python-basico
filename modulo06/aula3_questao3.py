49528246ed25799f7dfecc72bd122bb40170ab51
Paulo Henrique <juniorhenrique.1@hotmail.com>
2024-05-24 15:50:37 -0300

    Update aula3_questao3.py

--- modulo06/aula3_questao3.py
+++ modulo06/aula3_questao3.py
@@ -1,31 +1,36 @@
-lista = [9, 2, -1, -4, -2, -3, 5, 6, -7, -4, -1, 6, 8, -3, -6,-4,-2,-4]
+import random
 
+lista= []
 fim = 0
 start = 0
 intervalo_maior = []
-intervalo = 0
-inicio = 0
-final = 0
+index_intervalo = 0
+index_inicio = 0
+index_final = 0
 
+for i in range(20):lista.append(random.randint(-10,10))
+
+#for para percorrer a lista 
 for y in range(len(lista)):
+    #if para saber se o elemento e negativo
     if lista[y] < 0:
-        start = y        
+        #se for negativo vai começar por ele
+        start = y     
+        #for para achar o fim 
         for x in range(len(lista[y:])):
             lista_auxiliar = lista[start:]
             if lista_auxiliar[x] > 0:
                 break
             else: fim = x + 1
-        if(start + fim) - start > intervalo:
-            intervalo = (start + fim) - start
-            inicio = start
-            final = start + fim
+        #if para salvar o index do maior intervalo
+        if(start + fim) - start > index_intervalo:
+            index_intervalo = (start + fim) - start
+            index_inicio = start
+            index_final = start + fim
 
-
+#mostra o resultado
 print(lista)
-print(lista[inicio:final:])
-
-del lista[inicio:final]
-
+del lista[index_inicio:index_final]
 print(lista)
 
 
