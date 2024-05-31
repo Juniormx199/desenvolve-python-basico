0c1fe9b6ef7dbf45b76a9c1a4819bac4b0fb6526
Paulo Henrique <juniorhenrique.1@hotmail.com>
2024-05-31 10:29:48 -0300

    ..

---
+++ modulo07/aula2_questao4.py
@@ -0,0 +1,31 @@
+
+def validador_senha(senha):
+
+    if len(senha) < 8:
+        return False
+    
+    contem_letra_maiuscula = False
+    contem_letra_minuscula = False
+    contem_numero = False
+    contem_caractere_especial = False
+
+    for i in senha:
+        if i.isupper():
+            contem_letra_maiuscula = True
+        if i.islower():
+            contem_letra_minuscula = True
+        if i.isnumeric():
+            contem_numero = True
+        if i in "!@#$%^&*(),.?\":{}|<>":
+            contem_caractere_especial = True
+
+    return contem_letra_maiuscula and contem_letra_minuscula and contem_numero and contem_caractere_especial
+ 
+
+
+senha1 = "Senha123@"
+senha2 = "senhafraca"
+senha3 = "Senha_fraca"
+print(validador_senha(senha1))
+print(validador_senha(senha2))
+print(validador_senha(senha3))
