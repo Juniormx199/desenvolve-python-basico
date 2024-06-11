9a434479ad0908687bbb6174a91fe8b2bd90e9e1
Paulo Henrique <juniorhenrique.1@hotmail.com>
2024-06-10 21:20:07 -0300

    ...

--- trabalho pratico/crud.py
+++ trabalho pratico/crud.py
@@ -1,49 +1,69 @@
 import os.path
 
-def ler_arquivo_usuarios():
-    with open("usuarios.txt", "r") as arquivo:
+arquivo_usuarios = "usuarios.txt"
+
+#Funcao para ler arquivo
+def ler_arquivo(nome_arquivo):
+    if not os.path.exists(nome_arquivo):
+        escrever_arquivo(nome_arquivo)
+    with open(nome_arquivo, "r") as arquivo:
         return arquivo.readlines()
 
-def escreve_arquivo_usuarios(linhas):
-    with open("usuarios.txt", "w") as arquivo:
+#Funcao para escrever arquivo 
+def escrever_arquivo(nome_arquivo , linhas=""):
+    with open(nome_arquivo, "w") as arquivo:
         arquivo.writelines(linhas)
-    
-def alterar_usuario(nome, senha , novo, opcao):
-    valido , index = validar_usuario(nome , senha)
-    if valido:
-        linhas = ler_arquivo_usuarios()
-        if opcao == "3":
-            linhas[index]= f"{nome},{novo}\n"
-        else:   
-            linhas[index]= f"{novo},{senha}\n"
-        escreve_arquivo_usuarios(linhas)
 
-def excluir_usuario(nome, senha):
-    valido , index = validar_usuario(nome , senha)
-    if valido:
-        linhas = ler_arquivo_usuarios()
-        linhas.pop(index)
-        escreve_arquivo_usuarios(linhas)
-
+#Funcao para validar se o usuario existe : parametro senha e opcional (se passar ele a funcao verifica a senha tambem)
 def validar_usuario(nome , senha = ""):
-    linhas = ler_arquivo_usuarios()
+    linhas = ler_arquivo(arquivo_usuarios)
     for linha in linhas:
         usuario_txt, senha_txt = linha.strip().split(",")
         if usuario_txt.lower() == nome.lower():
             if senha == "":
+                print("Usuário já existe")
                 return True
             else: 
                 if (senha_txt == senha):
-                    return True , linhas.index(f"{nome},{senha}\n")
-    return False , -1
+                    return True
+                else:
+                    print("Usuario ou senha incorretos !!!")
+    return False
 
+#Funcao para adicionar um novo usuario
 def inserir_usuarios(nome, senha):
     if not validar_usuario(nome):
-        with open("usuarios.txt", "a") as arquivo:
-            arquivo.write(f"{nome},{senha}\n")
-            print("Usuário cadastrado")
+        linhas = ler_arquivo(arquivo_usuarios)
+        linhas.append(f"{nome},{senha}\n")
+        escrever_arquivo(arquivo_usuarios , linhas)
+        print("Usuário cadastrado !!!")
+
+#Funcao para alterar o usuario : tem duas opcoes = 3 altera a senha / 4 altera o nome (so pode alterar se fornecer usuario e senha atual)
+def alterar_usuario(nome, senha , novo, opcao):
+    if validar_usuario(nome , senha):
+        linhas = ler_arquivo(arquivo_usuarios)
+        index = linhas.index(f"{nome},{senha}\n")
+        if opcao == "3":
+            linhas[index]= f"{nome},{novo}\n"
+        else:   
+            linhas[index]= f"{novo},{senha}\n"
+        escrever_arquivo(arquivo_usuarios , linhas)
+        print("Usuario Alterado !!!")
+        return True
     else:
-        print("Usuário já existe")
+        return False
+
+#Funcao para excluir o usuario (so pode excluir se fornecer usuario e senha atual)
+def excluir_usuario(nome, senha):
+    if validar_usuario(nome , senha):
+        linhas = ler_arquivo(arquivo_usuarios)
+        index = linhas.index(f"{nome},{senha}\n")
+        linhas.pop(index)
+        escrever_arquivo(arquivo_usuarios , linhas)
+        print("Usuario excluido !!!")
+
+
+
 
 
 
@@ -65,19 +85,20 @@ while True:
     if opcao == "1":
         if validar_usuario(nome, senha):
             print("Login efetuado com sucesso!!!")
-        else:
+        else: 
             print("Usuario ou senha incorretos!!!")
 
     if opcao == "2":
         inserir_usuarios(nome, senha)
-
+            
     if opcao == "3":
         alterar_usuario(nome, senha ,input("Nova Senha: "), opcao)
-    
+
     if opcao == "4":
         alterar_usuario(nome, senha ,input("Novo nome: "), opcao)
 
     if opcao == "5":
         excluir_usuario(nome, senha)
+
 
     
